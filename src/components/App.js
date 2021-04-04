e0d0373fada546e1cad4f1c3da925877e92e6572
Olena Stadnyk <lenesx@gmail.com>
2021-04-04 21:41:52 +0300

    add fixed code

--- src/components/App.js
+++ src/components/App.js
@@ -7,16 +7,12 @@ import shortid from 'shortid';
 class App extends Component {
 state = {
    contacts: [
-    // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
+],
   filter: '',
 }
   
   formSubmitHandler = ({ name, number }) => {
-    if (this.state.contacts.some(el => el.name === name)) {
+    if (this.state.contacts.find((item) => item.name.toLowerCase() === name.toLowerCase())) {
       alert(`${name} is already in contacts.`);
     } else {
       const newContact = { id: shortid.generate(), name, number };
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -36,6 +36,9 @@ class ContactForm extends Component{
            <input
                             type='text'
                             name='name'
+                            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+                            required
                             id={this.nameInputId}
                             value={this.state.name}
                             onChange={this.handleChange} />
